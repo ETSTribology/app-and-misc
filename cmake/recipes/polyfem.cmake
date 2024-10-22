c4d150238a9bd12dccdc90085f1f6c2cd96b364e
Antoine Boucher <antoine.bou13@gmail.com>
2024-10-22 12:49:07 -0400

    Update polyfem.cmake

--- cmake/recipes/polyfem.cmake
+++ cmake/recipes/polyfem.cmake
@@ -11,9 +11,13 @@ include(FetchContent)
 FetchContent_Declare(
     polyfem
     GIT_REPOSITORY https://github.com/polyfem/polyfem.git
-    GIT_TAG c02e6e26c851e08be5225cab13c552a30707d503
+    GIT_TAG a3ef30c246316e79ad0bc02514a0cf2772a58cbf
     GIT_SHALLOW FALSE
 )
-FetchContent_MakeAvailable(polyfem)
+# Ensure USE_MKL is off for polysolve
+add_definitions(-DUSE_MKL=OFF)
+set(FETCHCONTENT_SOURCE_DIR_POLYFEM ${CMAKE_BINARY_DIR}/polyfem)
+set(FETCHCONTENT_UPDATES_MKL OFF)
 
+FetchContent_MakeAvailable(polyfem)
 
